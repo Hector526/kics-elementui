f921da4462772b330ac1469221948fe27e5b6b49
Hector <wolfares@163.com>
2020-05-13 14:44:29 +0800

    引入Element-UI，使用部分引入方法，修改babel.config.js中presets，解决报错问题

--- kics-elementui/src/main.js
+++ kics-elementui/src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
+
+import { Button, ButtonGroup } from "element-ui";
+
+import "element-ui/lib/theme-chalk/index.css";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+Vue.use(Button);
+Vue.use(ButtonGroup);
 
 Vue.config.productionTip = false;
 
