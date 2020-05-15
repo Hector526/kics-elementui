48489be254e5cf7efd1b5d3a531a9f6502a0183b
Hector <wolfares@163.com>
2020-05-15 16:38:49 +0800

    新增登录页路由和登录页部分样式

---
+++ kics-elementui/src/views/login/components/loginFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-footer-body">
+    <footer class="login-footer">
+      <div class="login-footer-copyright">Copyright © 科达科技智能云技术部出品</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login-footer-body {
+  padding: 0px;
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 14px;
+  background: #f0f2f5;
+  flex: 0 0 auto;
+
+  .login-footer {
+    margin: 48px 0 24px;
+    padding: 0 16px;
+    text-align: center;
+
+    .login-footer-copyright {
+      color: rgba(0, 0, 0, 0.45);
+      font-size: 14px;
+    }
+  }
+}
+</style>
---
+++ kics-elementui/src/views/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="user-layout-container">
+    <div class="user-layout-content"></div>
+    <login-footer></login-footer>
+  </div>
+</template>
+
+<script>
+import loginFooter from "./components/loginFooter";
+export default {
+  name: "login",
+  components: {
+    loginFooter
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.user-layout-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: auto;
+  min-width: 768px;
+  background: #f0f2f5;
+  background-image: url("../../assets/background.svg");
+  background-repeat: no-repeat;
+  background-position: center 110px;
+  background-size: 100%;
+
+  .user-layout-content {
+    padding: 72px 0 24px;
+    flex: 1 1;
+  }
+}
+</style>
