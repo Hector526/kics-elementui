2887500760f66674f83cfcd60fa9a99b15b702cd
Hector526 <wolfares@163.com>
2020-05-17 23:06:28 +0800

    删除app.vue中冗余css

--- kics-elementui/src/App.vue
+++ kics-elementui/src/App.vue
@@ -5,18 +5,6 @@
 </template>
 
 <style lang="less">
-body {
-  margin: 0;
-  color: rgba(0, 0, 0, 0.65);
-  font-size: 14px;
-  font-family: -apple-system, BlinkMacSystemFont, segoe ui, Roboto,
-    helvetica neue, Arial, noto sans, sans-serif, apple color emoji,
-    segoe ui emoji, segoe ui symbol, noto color emoji;
-  font-variant: tabular-nums;
-  line-height: 1.5715;
-  background-color: #fff;
-  font-feature-settings: "tnum", "tnum";
-}
 #app {
   height: 100%;
 }
